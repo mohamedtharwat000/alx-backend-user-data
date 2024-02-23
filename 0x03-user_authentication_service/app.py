627743694d87ce6c47e4926184286e248d19fba2
mohamedtharwat000 <mohamed.sarwat.000@gmail.com>
2024-02-23 19:37:12 +0200

    Create app.py

---
+++ 0x03-user_authentication_service/app.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+"""flask app"""
+
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def home():
+    """home page"""
+    return jsonify({"message": "Bienvenue"})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
