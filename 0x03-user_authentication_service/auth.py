62aa347fc1e1a86da33126c9012faa3430724c2c
mohamedtharwat000 <mohamed.sarwat.000@gmail.com>
2024-02-23 19:52:32 +0200

    Update auth.py

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -34,9 +34,6 @@ class Auth:
         """Validate login credentials."""
         try:
             user = self._db.find_user_by(email=email)
-            return (
-                bcrypt.checkpw(password.encode(),
-                               user.hashed_password.encode())
-            )
+            return bcrypt.checkpw(password.encode(), user.hashed_password)
         except NoResultFound:
             return False
