87dcb67b5948276dc70d64b8e9d274b1c13bb832
mohamedtharwat000 <mohamed.sarwat.000@gmail.com>
2024-02-23 16:54:25 +0200

    Update user.py

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -2,7 +2,7 @@
 
 """This module contains the User class."""
 
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Sequence, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
@@ -12,7 +12,7 @@ class User(Base):
     """This class represents a user."""
     __tablename__ = 'users'
 
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, Sequence('user_id_seq'), primary_key=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250))
