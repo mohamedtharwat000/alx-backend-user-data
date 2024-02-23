0c1348b18bba58844b56631afb6409c7dab37b67
mohamedtharwat000 <mohamed.sarwat.000@gmail.com>
2024-02-23 19:05:41 +0200

    Update auth.py

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -3,8 +3,29 @@
 """Auth module"""
 
 import bcrypt
+from user import User
+from db import DB
 
 
 def _hash_password(password: str) -> bytes:
     """Hash a password"""
     return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """Register a user
+        """
+        user = self._db.find_user_by(email=email)
+        if user is not None:
+            raise ValueError(f"User {email} already exists")
+
+        hashed_password = _hash_password(password)
+        user = self._db.add_user(email, hashed_password)
+        return user
