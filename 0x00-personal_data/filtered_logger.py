f80692c40b3989b101dd79245a17b9e509a53d39
mohamedtharwat000 <mohamed.sarwat.000@gmail.com>
2024-04-01 23:24:03 +0200

    Update filtered_logger.py

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
 """This module contains the User class."""
 
-import re
 
-def filter_datum(fields, redaction, message, separator):
-    return re.sub(r'(?<=(?:^|{0}))(?:(?:{1})(?={0}))|(?:^|{0})(?:{1})(?={0}|$)'.format(re.escape(separator), '|'.join(map(re.escape, fields))), redaction, message)
+import re
+from typing import List
+
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
+    return re.sub(r'(?<=(?:^|{0}))(?:(?:{1})(?={0}))|'
+                  r'(?:^|{0})(?:{1})(?={0}|$)'.format(
+                      re.escape(separator),
+                      '|'.join(map(re.escape, fields))),
+                  redaction, message)
