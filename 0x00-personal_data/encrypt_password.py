bb0f6da0ad9845aeb30f13063c846cc5586dc8e1
mohamedtharwat000 <mohamed.sarwat.000@gmail.com>
2024-04-01 23:31:40 +0200

    Create encrypt_password.py

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" Encrypting passwords """
+
+import bcrypt
+
+def hash_password(password: str) -> bytes:
+    """Hashes the input password using bcrypt."""
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+    return hashed_password
